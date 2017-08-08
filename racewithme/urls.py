738f11ee85a97b14cd7736015198b3916cbde3b9
Richard Giddings <mail@richardgiddings.co.uk>
2017-08-08 14:30:27 +0100

    Finish adding user urls to project

--- racewithme/urls.py
+++ racewithme/urls.py
@@ -36,5 +36,10 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, name='logout'),
 
     # password reset
-    url(r'^password_reset/$', auth_views.password_reset, {'password_reset_form': MyPasswordResetForm}, name='password_reset')
+    url(r'^password_reset/$', auth_views.password_reset, {'password_reset_form': MyPasswordResetForm}, 
+        name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$)', 
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
