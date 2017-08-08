f3bf066c11e0495db4d5dcb75ab2f662745d722e
Richard Giddings <mail@richardgiddings.co.uk>
2017-08-08 16:33:13 +0100

    Add template and static directories to base settings

--- racewithme/settings/base.py
+++ racewithme/settings/base.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'racewithme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,3 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), # project 
+]
