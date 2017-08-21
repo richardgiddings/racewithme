797566cce0efe9df455082b53e1a2a0c96dcb9d7
Richard Giddings <mail@richardgiddings.co.uk>
2017-08-21 11:58:13 +0100

    Return profile as a form

--- main/views.py
+++ main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from users.models import Profile
+from .forms import UserProfileForm
 
 def user_profile(request):
 
     profile = Profile.objects.get(user=request.user)
-
-    print(profile.user.username)
+    form = UserProfileForm(instance=profile)
 
     return render(request, template_name='main/user_profile.html', 
-                  context={'profile': profile})
+                  context={'form': form})
