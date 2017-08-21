6d00597acca400c8c4602b87351ca1da101731c7
Richard Giddings <mail@richardgiddings.co.uk>
2017-08-21 17:42:58 +0100

    Added Location, Race and UserRace models inlucding admin

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Distance
+from .models import Distance, Location, Race, UserRace
 
 admin.site.register(Distance)
+admin.site.Register(Location)
+admin.site.register(Race)
+admin.site.register(UserRace)
--- main/models.py
+++ main/models.py
@@ -1,23 +1,79 @@
 from django.db import models
-
-
-#class Race(models.Model):
-"""
-A Race listed on the website for people to join. This
-contains the basic information about the race.
-"""
-#race_name = models.CharField()
-#location = models.CharField()
-# distance = models.CharField()
-#race_site_link = models.URLField()
+from django.contrib.auth.models import User
 
 #class UserSettings(models.Model):
+
+class UserRace(models.Model):
+    """
+    A race that a user is attending and the details
+    for this user
+    """
+    user = models.ForeignKey(
+                        User,
+                        models.SET_NULL,
+                        blank=True,
+                        null=True)
+    race = models.ForeignKey(
+                        'Race',
+                        models.SET_NULL,
+                        blank=True,
+                        null=True)
+
+    # no times if this ticked
+    just_for_fun = models.BooleanField()
     
+    # target time
+    target_hours = models.IntegerField()
+    target_minutes = models.IntegerField()
+    target_seconds = models.IntegerField()
 
-#class UserRace(models.Model):
+    # time acheived
+    achieved_hours = models.IntegerField()
+    acheveed_minutes = models.IntegerField()
+    achieved_seconds = models.IntegerField()
 
+    # link to external race results and photos
+    race_results_external = models.URLField()
+    race_photos_external = models.URLField()
+
+    # ADD PHOTO UPLOAD LATER !!!!!!!!
+
+class Race(models.Model):
+    """
+    A Race listed on the website for people to join. This
+    contains the basic information about the race.
+    """
+    race_name = models.CharField(max_length=50)
+    race_location = models.ForeignKey(
+                                'Location', 
+                                models.SET_NULL, 
+                                blank=True, 
+                                null=True)
+    race_distance = models.ForeignKey(
+                                'Distance', 
+                                models.SET_NULL, 
+                                blank=True, 
+                                null=True)
+    race_site_link = models.URLField()
+    race_date = models.DateField(auto_now_add=True)
+    race_time = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.race_name
+
+class Location(models.Model):
+    """
+    A Race location
+    """
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Distance(models.Model):
+    """
+    A race distance
+    """
     description = models.CharField(max_length=25)
 
     def __str__(self):
