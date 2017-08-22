f02ca43f8f38d695e4e021070a5fa02785034dbf
Richard Giddings <mail@richardgiddings.co.uk>
2017-08-22 09:25:36 +0100

    Correct case of register in admin.py

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Distance, Location, Race, UserRace
 
 admin.site.register(Distance)
-admin.site.Register(Location)
+admin.site.register(Location)
 admin.site.register(Race)
 admin.site.register(UserRace)
