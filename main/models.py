f6165678038a8f9034a19126598173320528bfb9
Richard Giddings <mail@richardgiddings.co.uk>
2017-08-23 16:35:50 +0100

    Allow distance to be blank

--- main/models.py
+++ main/models.py
@@ -84,7 +84,7 @@ class Distance(models.Model):
     """
     A race distance
     """
-    description = models.CharField(max_length=25)
+    description = models.CharField(max_length=25, null=True, blank=True)
 
     def __str__(self):
         return self.description
