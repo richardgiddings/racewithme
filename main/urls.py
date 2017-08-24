076a38ddf5dcb7d7eace1a832247b9ecf34075bb
Richard Giddings <mail@richardgiddings.co.uk>
2017-08-24 18:11:16 +0100

    Start the interested tab

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^user_profile/', views.user_profile, name='user_profile'),
     url(r'^races/', views.races, name='races'),
+    url(r'^interested/', views.interested, name='interested')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from users.models import Profile
-from .models import Race
+from .models import Race, UserRace
 from .forms import UserProfileForm
 
 def user_profile(request):
@@ -17,4 +17,19 @@ def races(request):
     races = Race.objects.order_by('race_date')
 
     return render(request, template_name='main/races.html',
+                  context={'races': races})
+
+def interested(request):
+    if request.method == 'POST':
+        # create a new UserRace instance pointing to Race for User
+        race_id = request.POST.get("race_id")
+        race = Race.objects.get(pk=race_id)
+        user_race = UserRace.objects.create_user_race(
+            request.user, race, '1')
+
+    # get all UserRaces for user that they are interested in
+    races = UserRace.objects.filter(user=request.user)
+    races = races.filter(status='1')
+    
+    return render(request, template_name='main/interested.html',
                   context={'races': races})
