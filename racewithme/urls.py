a2ff2885d185995712cf32e126527801e42f2c00
Richard Giddings <mail@richardgiddings.co.uk>
2018-03-07 12:00:49 +0000

    Login page is now the root page for site.

--- racewithme/urls.py
+++ racewithme/urls.py
@@ -31,8 +31,8 @@ urlpatterns = [
         user_views.activate, name='activate'),
 
     # auth
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^home/$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 
     # password reset
